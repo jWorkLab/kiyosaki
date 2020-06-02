79caf81bf20a4bd9824ee81d1d8c26c602dd7274
jWorkLab <jWorkLab@yandex.ru>
2020-06-02 17:02:27 +0300

    Commit

--- src/js/main.js
+++ src/js/main.js
@@ -266,3 +266,15 @@ $(document).ready(function() {
 });
 
 
+$(document).ready(function() {
+    $('.callme-button').on('click', function () {
+        $('.callme-form').toggleClass('active');
+    });
+    $(document).mouseup(function (e){ // событие клика по веб-документу
+        var div = $(".callme-form"); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            && div.has(e.target).length === 0) { // и не по его дочерним элементам
+            div.removeClass('active'); // скрываем его
+        }
+    });
+});
