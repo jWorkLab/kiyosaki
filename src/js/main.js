cb53396c1197ae4c87b31dcb00ee5aec69e3042c
jWorkLab <jWorkLab@yandex.ru>
2020-06-02 15:58:09 +0300

    Commit

--- src/js/main.js
+++ src/js/main.js
@@ -66,6 +66,7 @@ $(document).ready(function(){
             }
         }
     });
+
     partnersSlider.on('changed.owl.carousel', function(event) {
         var items     = event.item.count;
         var item      = event.item.index;
